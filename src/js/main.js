8c5c1e4fa64be228d4f7f8c5e2d0ae8eb1b0ef63
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-10-23 23:58:42 +0300

    menu.json fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import createFoodList from '../templates/food-list.hbs';
-import objString from './menu.js';
+import objString from '../menu.json';
 import './themes';
 const foodListRef = document.querySelector('.js-menu');
 const foodListHtml = objString.map(el => createFoodList(el)).join('');
